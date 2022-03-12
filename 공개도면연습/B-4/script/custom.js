d8637ae56277b74f789b4c7b0217d14c78cb72b7
Eunseo Choi <yses9296@gmail.com>
2022-03-12 20:20:26 +0900

    B-4

---
+++ 공개도면연습/B-4/script/custom.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    $(".submenu").hide();
+    $("nav").mouseover(function(){
+       $(".submenu").stop().slideDown(300); 
+    });
+    $("nav").mouseout(function(){
+       $(".submenu").stop().slideUp(300); 
+    });
+});
+
+var currentIndex = 0;
+var position;
+
+setInterval(function(){
+    if(currentIndex < 2){
+        currentIndex++;
+    }
+    else {
+        currentIndex = 0;
+    }
+    
+    position = currentIndex * (-1200)+"px";
+    
+    $(".slideList").animate({left:position}, 300);
+}, 3000);
+
+$(".popup").click(function(){
+    $(".layout_bg").css("display", "block");
+});
+$(".close").click(function(){
+    $(".layout_bg").css("display", "none");
+});
